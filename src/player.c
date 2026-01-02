f6c289cc18950071ee58963696308dea8f4928bd
Mark Williams <pndaRN.dev@proton.me>
2026-01-01 18:52:05 -0600

    added player_update function

--- src/player.c
+++ src/player.c
@@ -11,3 +11,20 @@ Player player_create(int screen_width, int screen_height) {
 
   return p;
 }
+
+void player_update(Player *p, const Uint8 *keystate, float deltaTime,
+    int screenWidth) {
+      if (keystate[SDL_SCANCODE_LEFT] || keystate[SDL_SCANCODE_A]) {
+      p->x -= p->speed * deltaTime;
+    }
+    if (keystate[SDL_SCANCODE_RIGHT] || keystate[SDL_SCANCODE_D]) {
+      p->x += p->speed * deltaTime;
+    }
+
+    if (p->x < 0) {
+      p->x = 0;
+    }
+    if (p->x + p->width > screenWidth) {
+      p->x = screenWidth - p->width;
+    }
+  }
--- src/player.h
+++ src/player.h
@@ -1,5 +1,6 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <SDL2/SDL.h>
 
 typedef struct {
   float x, y;
@@ -9,5 +10,8 @@ typedef struct {
 } Player;
 
 Player player_create(int screen_width, int screen_height);
+
+void player_update(Player *p, const Uint8 *keystate, float deltaTime,
+  int screenWidth);
 
 #endif
