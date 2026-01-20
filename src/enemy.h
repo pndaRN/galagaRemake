83f306775e88a6b8de58610e3392f698188db2d1
Mark Williams <pndaRN.dev@proton.me>
2026-01-19 18:58:50 -0600

    added enemy state

--- src/enemy.h
+++ src/enemy.h
@@ -4,11 +4,23 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
+typedef enum {
+  ENEMY_ENTERING,
+  ENEMY_HOLDING,
+  ENEMY_DIVING,
+  ENEMY_RETURNING
+} EnemyState;
+
 typedef struct {
   float x, y, speed;
   int width, height;
   bool active;
+  EnemyState state;
+  int currentWaypoint;
+  SDL_FPoint path[10];
+  int pathLength;
 } Enemy ;
+
 
 Enemy enemy_init(int screen_width, int screen_height);
 
