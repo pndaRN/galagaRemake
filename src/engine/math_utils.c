f6befdc6e15eca16ebcdeeaa1e94ff328cd10e36
Mark Williams <pndaRN.dev@proton.me>
2026-04-24 14:03:52 -0500

    added bezier_tangent to math utils

--- src/engine/math_utils.c
+++ src/engine/math_utils.c
@@ -16,3 +16,21 @@ SDL_FPoint bezier_point(SDL_FPoint p0, SDL_FPoint p1, SDL_FPoint p2,
 
   return result;
 }
+
+SDL_FPoint bezier_tangent(SDL_FPoint p0, SDL_FPoint p1, SDL_FPoint p2, 
+                          SDL_FPoint p3, float t) {
+  float u = 1.0f - t;
+  float uu = u * u; 
+  float tt = t * t; 
+  float ut6 = 6.0f * u * t; 
+
+  SDL_FPoint result; 
+  result.x = 3.0f * uu * (p1.x - p0.x)
+           + ut6 * (p2.x - p1.x) 
+           + 3.0f * tt * (p3.x - p2.x);
+  result.y = 3.0f * uu * (p1.y - p0.y)
+           + ut6 * (p2.y - p1.y) 
+           + 3.0f * tt * (p3.y - p2.y);
+
+  return result;
+}
